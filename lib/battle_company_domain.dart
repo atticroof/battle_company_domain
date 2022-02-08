78aee03af6c1a2ba7dd2cc401cef556b66ea2d37
Vladimir Moskalev <vmoskalev@riseup.net>
2022-02-08 14:22:07 +0300

    initial

---
+++ lib/battle_company_domain.dart
@@ -0,0 +1,3 @@
+library battle_company_domain;
+
+export 'objects.dart';
---
+++ test/battle_company_domain_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:battle_company_domain/battle_company_domain.dart';
+
+void main() {
+  test('just scratch', () {
+    final test = Track();
+  });
+}
